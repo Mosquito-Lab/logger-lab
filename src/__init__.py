9f5dba0067767ae0aa6bcda924f1a2f9b59a668f
Mosquito-Lab <MosquitoLab2024@protonmail.com>
2026-05-05 11:47:01 +0200

    v0.1.0
    Main init

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+""" Project D-bug """
+
+__version__ = "0.1.0"
+__author__ = "Mosquito Lab (Cyber-Smoke)"
+__email__ = "<mosquitolab2024@protonmail.com>"
+__github__ = "https://github.com/Mosquito-Lab/logger-lab"
+__status__ = "Development"
